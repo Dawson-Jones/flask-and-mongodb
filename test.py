bc1b9fed82740acbfc8ecbf4b4525b7b5e780ef9
PhoeniXWhite <957360688@qq.com>
2020-01-09 16:00:14 +0800

    add route for find panel data

---
+++ test.py
@@ -0,0 +1,23 @@
+import time
+from yoozen_db.basic.db import *
+
+
+def agg():
+    limit = list(el_config_collection.aggregate([
+        {'$match': {'cell_type': 'mono'}},
+        {'$group': {
+            '_id': '$display_mode',
+            'limit': {'$sum': 1}
+        }}
+    ]))
+    print(limit)
+    return 'yes'
+
+
+def query():
+    res = panel_collection.find({}).sort('create_time', -1)
+    print(len(list(res)))
+
+
+if __name__ == '__main__':
+    query()
